717ae682b8e0e1f4eb514a4c4041d0e0de47c3ca
Ali Emre Senel <aliemre@senel.biz>
2026-04-17 21:14:54 +0200

    CUDA13 build flags and torch 2.11 dispatch fixes

--- setup.py
+++ setup.py
@@ -1,9 +1,35 @@
 import os.path as osp
+import os
 
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 ROOT = osp.dirname(osp.abspath(__file__))
+
+
+def _nvcc_arch_flags():
+    """Build CUDA arch flags from env; default avoids deprecated sm_70 on CUDA 13."""
+    arch_list = os.environ.get("MEGASAM_CUDA_ARCHES") or os.environ.get("TORCH_CUDA_ARCH_LIST")
+    if arch_list:
+        entries = [x.strip().replace("+PTX", "") for x in arch_list.replace(";", ",").split(",") if x.strip()]
+        flags = []
+        for e in entries:
+            major_minor = e.split(".")
+            if len(major_minor) == 1:
+                sm = f"{major_minor[0]}0"
+            else:
+                sm = f"{major_minor[0]}{major_minor[1]}"
+            flags.append(f"-gencode=arch=compute_{sm},code=sm_{sm}")
+        if flags:
+            return flags
+
+    return [
+        "-gencode=arch=compute_75,code=sm_75",
+        "-gencode=arch=compute_80,code=sm_80",
+        "-gencode=arch=compute_86,code=sm_86",
+        "-gencode=arch=compute_89,code=sm_89",
+        "-gencode=arch=compute_90,code=sm_90",
+    ]
 
 setup(
     name='droid_backends',
@@ -21,13 +47,9 @@ setup(
                 'cxx': ['-O3'],
                 'nvcc': [
                     '-O3',
-                    # '-gencode=arch=compute_60,code=sm_60',
-                    # '-gencode=arch=compute_61,code=sm_61',
-                    '-gencode=arch=compute_70,code=sm_70',
-                    '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_80,code=sm_80',
-                    '-gencode=arch=compute_86,code=sm_86',
-                ],
+                    '-diag-suppress=20011',
+                    '-diag-suppress=20014',
+                ] + _nvcc_arch_flags(),
             },
         ),
     ],
@@ -56,13 +78,9 @@ setup(
                 'cxx': ['-O2'],
                 'nvcc': [
                     '-O2',
-                    # '-gencode=arch=compute_60,code=sm_60',
-                    # '-gencode=arch=compute_61,code=sm_61',
-                    '-gencode=arch=compute_70,code=sm_70',
-                    '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_80,code=sm_80',
-                    '-gencode=arch=compute_86,code=sm_86',
-                ],
+                    '-diag-suppress=20011',
+                    '-diag-suppress=20014',
+                ] + _nvcc_arch_flags(),
             },
         ),
     ],
